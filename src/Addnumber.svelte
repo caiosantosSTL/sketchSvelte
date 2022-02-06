1d2bddbb531ae32c0297978dbe4fe3717958ea70
Caio Santos da Silva <vorreicaio@hotmail.com>
2022-02-06 17:23:23 -0600

    new update concepts

---
+++ src/Addnumber.svelte
@@ -0,0 +1,14 @@
+<script>
+    let numbers = [1, 2, 3, 4]
+
+    function addNumbers(params) {
+        numbers = [...numbers, numbers.length + 1]
+
+    }
+    $: sum = numbers.reduce((t, n)=> t + n, 0)
+</script>
+
+<main>
+    <p>{numbers.join(' + ')} = {sum}</p>
+    <button on:click={addNumbers}>add</button>
+</main>
--- src/Pagex.svelte
+++ src/Pagex.svelte
@@ -2,11 +2,21 @@
     import Newpage from "./Newpage.svelte";
     import Inputapi from "./Inputapi.svelte";
     import Times from "./Times.svelte";
+    import Add from "./Addnumber.svelte";
+    import If from "./Ifs.svelte";
+    import If2 from "./Ifs2.svelte";
 </script>
 
-<Newpage cont={0}/>
-<Newpage cont={10}/>
+<main>
+    <Newpage cont={0}/>
+    <Newpage cont={10}/>
+    
+    <Inputapi/>
+    
+    <Times/>
+    
+    <Add/>
+    <If/>
+    <If2 x={12}/>
 
-<Inputapi/>
-
-<Times/>
+</main>
